8464d09d0ce899116f7724d4d1251785526a13a6
cris8301 <crisvasre@gmail.com>
2026-05-02 19:52:31 -0500

    Add meme dictionary and user interaction

---
+++ diccionario-de-memes.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "CREEPY": "Algo aterrador o siniestro",
+            }
+
+nombre = input("hola, ecriba su nombre: ")
+print("Mucho gusto", nombre,)
+word = input("escriba una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(word + ": " + meme_dict[word])
+else:
+    print(word + "lo siento, no está en el diccionario.")
