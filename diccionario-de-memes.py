8bdd8c89dafa0b11e52dd94947161fe2310166b5
cris8301 <crisvasre@gmail.com>
2026-05-09 10:52:32 -0500

    palabras nueva añadidas

--- diccionario-de-memes.py
+++ diccionario-de-memes.py
@@ -2,7 +2,12 @@ meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
             "CREEPY": "Algo aterrador o siniestro",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "MAMBO": "Personaje salido de Uma Musume Pretty Derby (nombre real (Matikanetannhauser)"
             }
+
+#No c me ocurre mas :v
 
 nombre = input("hola, ecriba su nombre: ")
 print("Mucho gusto", nombre,)
@@ -11,4 +16,4 @@ word = input("escriba una palabra que no entiendas (¡con mayúsculas!): ")
 if word in meme_dict.keys():
     print(word + ": " + meme_dict[word])
 else:
-    print(word + "lo siento, no está en el diccionario.")
+    print(word + " lo siento, no está en el diccionario.")
